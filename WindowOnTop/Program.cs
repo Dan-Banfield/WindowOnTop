75fbcbf8544fe85931e76c5bc69ff9ee1901b4db
Dan Banfield <dandbanfield@gmail.com>
2022-06-22 20:40:34 +0100

    Removed the default form and created a custom application context to act as the base for the main application.

--- WindowOnTop/Program.cs
+++ WindowOnTop/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowOnTop
@@ -16,7 +13,12 @@ namespace WindowOnTop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new WindowOnTop());
         }
+    }
+
+    public class WindowOnTop : ApplicationContext
+    {
+
     }
 }
