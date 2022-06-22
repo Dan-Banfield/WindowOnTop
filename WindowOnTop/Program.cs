5687f9b623e7e1b9334522e63edee7cb57f61b98
Dan Banfield <dandbanfield@gmail.com>
2022-06-22 21:05:26 +0100

    Added an application icon, and code to register a global hot key as well as to set up a tray application.

--- WindowOnTop/Program.cs
+++ WindowOnTop/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Windows.Forms;
+using WindowOnTop.Properties;
+using System.Runtime.InteropServices;
 
 namespace WindowOnTop
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
@@ -19,6 +18,158 @@ namespace WindowOnTop
 
     public class WindowOnTop : ApplicationContext
     {
+        private NotifyIcon trayIcon;
 
+        public WindowOnTop()
+        {
+            RegisterGlobalHotKey();
+            InitializeTrayIcon();
+        }
+
+        private void RegisterGlobalHotKey()
+        {
+            KeyboardHook keyboardHook = new KeyboardHook();
+
+            keyboardHook.KeyPressed += KeyboardHook_KeyPressed;
+            keyboardHook.RegisterHotKey(ModifierKeys.Control | ModifierKeys.Shift,
+            Keys.O);
+        }
+
+        private void KeyboardHook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            // TODO: Set the current foreground window's state to topmost.
+        }
+
+        private void InitializeTrayIcon()
+        {
+            trayIcon = new NotifyIcon()
+            {
+                Text = "WindowOnTop\nCtrl+Shift+O",
+                Icon = Resources.AppIcon,
+
+                ContextMenu = new ContextMenu(new MenuItem[] 
+                {
+                    new MenuItem("Exit", Exit)
+                }),
+
+                Visible = true
+            };
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+
+    public sealed class KeyboardHook : IDisposable
+    {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private class Window : NativeWindow, IDisposable
+        {
+            private static int WM_HOTKEY = 0x0312;
+
+            public Window()
+            {
+                this.CreateHandle(new CreateParams());
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                base.WndProc(ref m);
+
+                if (m.Msg == WM_HOTKEY)
+                {
+                    Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+                    ModifierKeys modifier = (ModifierKeys)((int)m.LParam & 0xFFFF);
+
+                    if (KeyPressed != null)
+                        KeyPressed(this, new KeyPressedEventArgs(modifier, key));
+                }
+            }
+
+            public event EventHandler<KeyPressedEventArgs> KeyPressed;
+
+            #region IDisposable Members
+
+            public void Dispose()
+            {
+                this.DestroyHandle();
+            }
+
+            #endregion
+        }
+
+        private Window _window = new Window();
+        private int _currentId;
+
+        public KeyboardHook()
+        {
+            _window.KeyPressed += delegate (object sender, KeyPressedEventArgs args)
+            {
+                if (KeyPressed != null)
+                    KeyPressed(this, args);
+            };
+        }
+
+        public void RegisterHotKey(ModifierKeys modifier, Keys key)
+        {
+            _currentId = _currentId + 1;
+
+            if (!RegisterHotKey(_window.Handle, _currentId, (uint)modifier, (uint)key))
+                throw new InvalidOperationException("Couldn’t register the hot key.");
+        }
+
+        public event EventHandler<KeyPressedEventArgs> KeyPressed;
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            for (int i = _currentId; i > 0; i--)
+            {
+                UnregisterHotKey(_window.Handle, i);
+            }
+
+            _window.Dispose();
+        }
+
+        #endregion
+    }
+
+    public class KeyPressedEventArgs : EventArgs
+    {
+        private ModifierKeys _modifier;
+        private Keys _key;
+
+        internal KeyPressedEventArgs(ModifierKeys modifier, Keys key)
+        {
+            _modifier = modifier;
+            _key = key;
+        }
+
+        public ModifierKeys Modifier
+        {
+            get { return _modifier; }
+        }
+
+        public Keys Key
+        {
+            get { return _key; }
+        }
+    }
+
+    [Flags]
+    public enum ModifierKeys : uint
+    {
+        Alt = 1,
+        Control = 2,
+        Shift = 4,
+        Win = 8
     }
 }
